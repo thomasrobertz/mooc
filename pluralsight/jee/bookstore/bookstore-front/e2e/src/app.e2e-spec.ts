4b67fc7b74578e95cbf1900baa9ddd9f083ae8d1
Thomas Robertz <thomas.robertz@googlemail.com>
2020-07-31 10:36:23 +0200

    Fresh ng app, course version is too outdated

--- pluralsight/jee/bookstore/bookstore-front/e2e/src/app.e2e-spec.ts
+++ pluralsight/jee/bookstore/bookstore-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bookstore-front!');
+    expect(page.getTitleText()).toEqual('bookstore-front app is running!');
   });
 
   afterEach(async () => {
--- pluralsight/jee/bookstore/bookstore-front/src/app/service/api/api.ts
+++
@@ -1,3 +0,0 @@
-export * from './book.service';
-import { BookService } from './book.service';
-export const APIS = [BookService];
