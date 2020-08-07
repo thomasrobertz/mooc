4f94b3d1b67460cd0cfc298b8a05883a72cacad6
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-07 12:24:06 +0200

    Initial commit

---
+++ pluralsight/ws/xml-fundamentals/1/ItemClass/ItemClass/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemClass
+{
+  public class Item
+  {
+    public string Description;
+    public float Price;
+  }
+}
