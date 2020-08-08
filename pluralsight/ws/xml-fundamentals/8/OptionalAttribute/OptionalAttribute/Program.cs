8aae28c31519a07cef05c0c8289d5a4ceaf36c47
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-08 12:39:30 +0200

    Chapter 8

---
+++ pluralsight/ws/xml-fundamentals/8/OptionalAttribute/OptionalAttribute/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml;
+
+namespace OptionalAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+            var xreader = XmlReader.Create(args[0], settings);
+            var xdoc = new XmlDocument();
+            xdoc.Load(xreader);
+            var sizeAttribute = xdoc.SelectSingleNode("/item/@size");
+            var stockroomAttribute = xdoc.SelectSingleNode(
+                "/item/@stockroom");
+            Console.WriteLine("size {0}",sizeAttribute!=null ?
+                sizeAttribute.Value : "not found");
+            Console.WriteLine("stockroom {0}",
+                stockroomAttribute!=
+                null ? stockroomAttribute.Value : "not found");
+        }
+    }
+}
---
+++ pluralsight/ws/xml-fundamentals/8/PartialDTD/PartialDTD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace PartialDTD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var settings = new XmlReaderSettings();
+          settings.DtdProcessing = DtdProcessing.Parse;
+          settings.ValidationType = ValidationType.None;
+          var xrdr = XmlReader.Create(args[0], settings);
+          XmlDocument xdoc = new XmlDocument();
+          xdoc.Load(xrdr);
+            var attribute =
+                (XmlAttribute)xdoc.SelectSingleNode(
+                "/items/item/@color");
+            Console.WriteLine("\"{0}\"", attribute.Value);
+        }
+    }
+}
