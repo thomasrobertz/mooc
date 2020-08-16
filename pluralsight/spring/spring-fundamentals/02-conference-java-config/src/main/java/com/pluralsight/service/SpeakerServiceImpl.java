9133b3c06e3ca892868d6014e2d1eb22a360d9de
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-16 20:01:33 +0200

    javax @PostConstruct

--- pluralsight/spring/spring-fundamentals/02-conference-java-config/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ pluralsight/spring/spring-fundamentals/02-conference-java-config/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -6,24 +6,29 @@ import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
+    @Autowired
     private SpeakerRepository repository;
 
     public SpeakerServiceImpl() {
         System.out.println("SpeakerServiceImpl no args ctor");
     }
 
-    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
         System.out.println("SpeakerServiceImpl repository ctor");
         repository = speakerRepository;
     }
 
-    //@Autowired
+    @PostConstruct
+    private void initialize() {
+        System.out.println("PostConstruct");
+    }
+
     public void setRepository(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl setter");
         this.repository = repository;
