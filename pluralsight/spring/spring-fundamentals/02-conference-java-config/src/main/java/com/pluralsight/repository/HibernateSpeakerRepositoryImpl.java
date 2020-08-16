697865c5b54059661c839ca313f40a1865dcc04c
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-16 20:15:42 +0200

    Bean Factory

--- pluralsight/spring/spring-fundamentals/02-conference-java-config/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ pluralsight/spring/spring-fundamentals/02-conference-java-config/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar calendar;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<>();
@@ -18,6 +24,8 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         speaker.setLastName("Smith");
 
         speakers.add(speaker);
+
+        System.out.println("cal: " + calendar.getTime());
 
         return speakers;
     }
