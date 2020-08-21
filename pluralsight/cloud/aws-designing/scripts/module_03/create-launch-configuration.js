3dc9c9713fa018bd8f54ab82f4f022bff6a2b7dd
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-21 11:12:17 +0200

    Prgrammatic Launch configuration

--- pluralsight/cloud/aws-designing/scripts/module_03/create-launch-configuration.js
+++ pluralsight/cloud/aws-designing/scripts/module_03/create-launch-configuration.js
@@ -1,20 +1,37 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'eu-central-1' })
 
 // Declare local variables
-// TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling();
 
 const lcName = 'hamsterLC'
 const roleName = 'hamsterLCRole'
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+const sgName = 'hamster_sg2'
+const keyName = 'hamster_key2'
 
 helpers.createIamRole(roleName)
 .then(profileArn => createLaunchConfiguration(lcName, profileArn))
 .then(data => console.log(data))
 
 function createLaunchConfiguration (lcName, profileArn) {
-  // TODO: Create a launch configuration
+  const params = {
+    IamInstanceProfile: profileArn,
+    ImageId: "ami-0e65dbc8fcc29f955",
+    InstanceType: "t2.micro",
+    LaunchConfigurationName: lcName,
+    KeyName: keyName,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: "IyEvYmluL2Jhc2gNCnN1ZG8gYXB0LWdldCB1cGRhdGUNCnN1ZG8gYXB0LWdldCAteSBpbnN0YWxsIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQgL2hvbWUvYml0bmFtaS9oYmZsDQpjaG93biAtUiBiaXRuYW1pOiAvaG9tZS9iaXRuYW1pL2hiZmwNCmNkIC9ob21lL2JpdG5hbWkvaGJmbA0Kc3VkbyBucG0gaQ0Kc3VkbyBucG0gcnVuIHN0YXJ0"
+  }
+
+  return new Promise((resolve, reject) => {
+    autoScaling.createLaunchConfiguration(params, (err, data) => {
+      if(err) reject(err)
+      else resolve(data)
+    }) 
+  }).catch(error => { console.log('caught: ', error.message); });
 }
