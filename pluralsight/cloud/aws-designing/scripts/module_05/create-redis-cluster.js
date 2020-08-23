e11f75747275f3eb98b9e21603e216354cc105b8
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-23 13:44:38 +0200

    Dynamo, RDS, and ElastiCache

--- pluralsight/cloud/aws-designing/scripts/module_05/create-redis-cluster.js
+++ pluralsight/cloud/aws-designing/scripts/module_05/create-redis-cluster.js
@@ -2,18 +2,27 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'eu-central-1' })
 
-// TODO: Create an elasticache object
+const ec = new AWS.ElastiCache()
 
 helpers.createSecurityGroup('hamster_redis_sg', 6379)
 .then(sgId => createRedisCluster('hamster', sgId))
 .then(data => console.log(data))
 
 function createRedisCluster (clusterName, sgId) {
-  // TODO: Create params object
+  const params = {
+    CacheClusterId: clusterName, 
+    CacheNodeType: 'cache.t2.micro',
+    Engine: 'redis',
+    NumCacheNodes: 1,
+    SecurityGroupIds: [sgId]
+  }
 
   return new Promise((resolve, reject) => {
-    // TODO: Create cache cluster
+    ec.createCacheCluster(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
