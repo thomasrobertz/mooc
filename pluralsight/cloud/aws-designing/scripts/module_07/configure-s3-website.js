dc8a5e9dbbc697d40300350a4ce9706249104557
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-24 12:50:12 +0200

    S3 Static Website

--- pluralsight/cloud/aws-designing/scripts/module_07/configure-s3-website.js
+++ pluralsight/cloud/aws-designing/scripts/module_07/configure-s3-website.js
@@ -6,13 +6,22 @@ AWS.config.update({ region: 'eu-central-1' })
 // Declare local variables
 const s3 = new AWS.S3()
 
-configureS3Site('/* TODO: Add your S3 bucket name */')
+configureS3Site('hamster-bucket-robertz-psc5')
 .then(data => console.log(data))
 
 function configureS3Site (bucketName) {
-  // TODO: Create params const object
-
+  const params = {
+    Bucket: bucketName,
+    WebsiteConfiguration: {
+      IndexDocument: {
+        Suffix: "index.html"
+      }
+    }
+  }
   return new Promise((resolve, reject) => {
-    // Call putBucketWebsite
+    s3.putBucketWebsite(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
