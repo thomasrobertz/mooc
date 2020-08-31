76ce470f17d22f2a7c3f2bac59f6355165ceacaf
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-31 13:52:37 +0200

    Configure to show details of customized endpoint

--- pluralsight/spring/spring-actuator/todo-app/src/main/java/io/schultz/dustin/todoapp/health/MaxMemoryHealthIndicator.java
+++ pluralsight/spring/spring-actuator/todo-app/src/main/java/io/schultz/dustin/todoapp/health/MaxMemoryHealthIndicator.java
@@ -3,7 +3,6 @@ package io.schultz.dustin.todoapp.health;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class MaxMemoryHealthIndicator implements HealthIndicator {
     public Health health() {
         boolean invalid = Runtime.getRuntime().maxMemory() < (100 * 1024 * 1024);
         Status status = invalid ? Status.DOWN : Status.UP;
-        //return Health.status(status).withDetail("response_code", HttpStatus.OK).build();
-        return Health.down().build();
+        return Health.status(status).build();
     }
 }
