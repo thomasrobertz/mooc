fc9cdfa1cff5a1cfb369d7e7e75a0e2fc4d4672a
Thomas Robertz <thomas.robertz@googlemail.com>
2020-08-31 13:41:58 +0200

    Custom health indicator test with down()

--- pluralsight/spring/spring-actuator/todo-app/src/main/java/io/schultz/dustin/todoapp/health/MaxMemoryHealthIndicator.java
+++ pluralsight/spring/spring-actuator/todo-app/src/main/java/io/schultz/dustin/todoapp/health/MaxMemoryHealthIndicator.java
@@ -3,6 +3,7 @@ package io.schultz.dustin.todoapp.health;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ public class MaxMemoryHealthIndicator implements HealthIndicator {
     public Health health() {
         boolean invalid = Runtime.getRuntime().maxMemory() < (100 * 1024 * 1024);
         Status status = invalid ? Status.DOWN : Status.UP;
-        return Health.status(status).build();
+        //return Health.status(status).withDetail("response_code", HttpStatus.OK).build();
+        return Health.down().build();
     }
 }
