970a1ec56413cece7eb9ef33aca59ff08d9c609a
Thomas Robertz <thomas.robertz@googlemail.com>
2020-09-13 11:36:44 +0200

    Added comments

--- freecodecamp/challenges/algorithms/2-inventory/inventory.js
+++ freecodecamp/challenges/algorithms/2-inventory/inventory.js
@@ -1,21 +1,28 @@
+// Solution to https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/inventory-update
+
 function updateInventory(arr1, arr2) {
 
+    // Create an array of just the product names
     let currentByKeys = arr1.map(i => i[1])
     let newItems = []
     
     if(arr1.length == 0) {
+        // Empty current inventory, just copy all items.
         arr1 = arr2
     } else {
 
+        // Bubble through each inventory, if the item exists update the count, otherwise add new.
         arr2.forEach(n => {
             arr1.forEach(c => {
                 if (currentByKeys.includes(n[1])) {
+                    // Item exists, update the count.
                     if(c[1] == n[1]) {
                         c[0] += n[0]
                     }
                 } 
                 else {
                     if(!newItems.includes(n)) {
+                        // Add new item. Becuase we are bubbling, add only once.
                         newItems.push(n)
                     }
                 }
@@ -23,6 +30,7 @@ function updateInventory(arr1, arr2) {
         })
     }
 
+    // Add the new items that were created above, sort by name and return result.
     return arr1.concat(newItems).sort((a, b) => {
         if(a[1] > b[1]) return 1;
         if(a[1] < b[1]) return -1;
