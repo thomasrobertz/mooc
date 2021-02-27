4142d58b51e8e49e6487447e33e004661d98c5a9
Thomas Robertz <thomas.robertz@googlemail.com>
2021-02-27 11:22:09 +0100

    Added Order and some @Value annotations to User

---
+++ pluralsight/spring/spring-spel/spel-demo/src/main/java/com/example/speldemo/data/Order.java
@@ -0,0 +1,55 @@
+package com.example.speldemo.data;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Order {
+
+    // Just demonstrating simple math in SpEL
+    @Value("#{100.55 + 500.75 + 400-66}")
+    double amount;
+
+    @Value("#{order.amount = 1000 ? order order.amount * 5 / 1000 : 0}")
+    double discount;
+
+    @Value("#{user.country == 'US' and user-timeZone == 'America/NewYork' ? 3 : 1}")
+    int daysToDeliver;
+
+    @Value("#{user.country}")
+    String origin;
+
+    
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getDaysToDeliver() {
+        return daysToDeliver;
+    }
+
+    public void setDaysToDeliver(int daysToDeliver) {
+        this.daysToDeliver = daysToDeliver;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+}
