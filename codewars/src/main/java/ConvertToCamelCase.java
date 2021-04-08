c7596bf51cabfca79a4cf7266f7a9ffafa095001
Thomas Robertz <thomas.robertz@googlemail.com>
2021-04-08 22:35:11 +0200

    CamelCase

--- codewars/src/main/java/ConvertToCamelCase.java
+++ codewars/src/main/java/ConvertToCamelCase.java
@@ -1,3 +1,28 @@
+import java.util.function.Function;
+import java.util.stream.Stream;
+
 // https://www.codewars.com/kata/517abf86da9663f1d2000003/
 public class ConvertToCamelCase {
+    public static void main(String[] args) {
+        String input = "the-stealth-warrior";
+        input = "The_Stealth_Warrior";
+        System.out.println(convert(input));
+    }
+
+    public static String changeCasing(String input, Function<String, String> caseFunction) {
+        return caseFunction.apply(input.substring(0, 1)) + input.substring(1);
+    }
+
+    public static String convert(String input) {
+        if (input.equals("")) {
+            return "";
+        }
+        boolean wasLower = (input.substring(0, 1).equals(input.substring(0, 1).toLowerCase()));
+        String result = Stream.of(input.split("\\-|_")).reduce("",
+                (i, p) -> i + changeCasing(p, s -> s.toUpperCase()));
+        if (wasLower) {
+            result = changeCasing(result, s -> s.toLowerCase());
+        }
+        return result;
+    }
 }
