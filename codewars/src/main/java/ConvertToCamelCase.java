865f4ea661d2ba0c59ba61fb0e185da1a4b2a7e4
trobertz <thomas.robertz@bcmsolutions.de>
2021-04-08 21:18:19 +0200

    Greed

---
+++ codewars/src/main/java/ConvertToCamelCase.java
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/517abf86da9663f1d2000003/
+public class ConvertToCamelCase {
+}
---
+++ codewars/src/main/java/PickPeaks.java
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/5279f6fe5ab7f447890006a7
+public class PickPeaks {
+}
