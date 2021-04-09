979f70a0edfd546a6f5976f65c6b92717284bae3
trobertz <thomas.robertz@bcmsolutions.de>
2021-04-09 17:30:50 +0200

    Thirteen

--- codewars/src/main/java/Thirteen.java
+++ codewars/src/main/java/Thirteen.java
@@ -1,51 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 // https://www.codewars.com/kata/564057bc348c7200bd0000ff
 public class Thirteen {
-    /*
-    * When you divide the successive powers of 10 by 13 you get the following remainders of the integer divisions:
+    public static void main(String[] args) {
+        long input = 1111111111;
+        System.out.println(thirt(input));
+    }
 
-1, 10, 9, 12, 3, 4 because:
+    public static long thirt(long n) {
 
-10 ^ 0 ->  1 (mod 13)
-10 ^ 1 -> 10 (mod 13)
-10 ^ 2 ->  9 (mod 13)
-10 ^ 3 -> 12 (mod 13)
-10 ^ 4 ->  3 (mod 13)
-10 ^ 5 ->  4 (mod 13)
-(For "mod" you can see: https://en.wikipedia.org/wiki/Modulo_operation)
+        List<String> numbers = Scanner.toList(String.valueOf(n));
+        Stack<String> numberStack = new Stack<String>();
+        numberStack.addAll(numbers);
 
-Then the whole pattern repeats. Hence the following method:
+        List<Long> moduloFactorsOf13 = decimalModuloFactorsOf(13, numbers.size());
 
-Multiply
+        Long result = moduloFactorsOf13.stream().reduce(0L, (i, p) ->
+            i += Long.parseLong(numberStack.pop()) * ((Number) p).longValue()
+        );
 
-the right most digit of the number with the left most number in the sequence shown above,
-the second right most digit with the second left most digit of the number in the sequence.
-The cycle goes on and you sum all these products. Repeat this process until the sequence of sums is stationary.
+        if (result == n) {
+            return result;
+        }
 
-Example:
-What is the remainder when 1234567 is divided by 13?
+        return thirt(result);
+    }
 
-7      6     5      4     3     2     1  (digits of 1234567 from the right)
-×      ×     ×      ×     ×     ×     ×  (multiplication)
-1     10     9     12     3     4     1  (the repeating sequence)
-Therefore following the method we get:
+    public static List<Long> decimalModuloFactorsOf(int number, int size) {
+        return Stream.iterate(0, n -> n + 1)
+            .map(n -> (long) Math.pow(10, n) % number)
+            .limit(size)
+            .collect(Collectors.toList());
+    }
 
-7×1 + 6×10 + 5×9 + 4×12 + 3×3 + 2×4 + 1×1 = 178
-
-We repeat the process with the number 178:
-
-8x1 + 7x10 + 1x9 = 87
-
-and again with 87:
-
-7x1 + 8x10 = 87
-
-From now on the sequence is stationary (we always get 87) and the remainder of 1234567 by 13 is the same as the remainder of 87 by 13 ( i.e 9).
-
-Task:
-Call thirt the function which processes this sequence of operations on an integer n (>=0). thirt will return the stationary number.
-
-thirt(1234567) calculates 178, then 87, then 87 and returns 87.
-
-thirt(321) calculates 48, 48 and returns 48
-    * */
+    public static class Scanner {
+        private String input;
+        public Scanner(String input) {
+            this.input = input;
+        }
+        public boolean hasNext() {
+            return input.length() > 0;
+        }
+        public String next() {
+            String next = input.substring(0, 1);
+            input = input.substring(1, input.length());
+            return next;
+        }
+        public static List<String> toList(String input) {
+            Scanner scanner = new Scanner(input);
+            List<String> result = new ArrayList<>();
+            while(scanner.hasNext()) {
+                result.add(scanner.next());
+            }
+            return result;
+        }
+    }
 }
