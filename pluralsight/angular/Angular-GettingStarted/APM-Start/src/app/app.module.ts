7e2591d7f1ee4018cb0aad59626634a703bebd64
trobertz <thomas.robertz@bcmsolutions.de>
2021-05-04 17:12:30 +0200

    Up to chapter 4

--- pluralsight/angular/Angular-GettingStarted/APM-Start/src/app/app.module.ts
+++ pluralsight/angular/Angular-GettingStarted/APM-Start/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ProductModule,
   ],
   bootstrap: [AppComponent]
 })
---
+++ pluralsight/angular/Angular-GettingStarted/APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProductListComponent } from './product-list.component';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+  ],
+  imports: [
+  ]
+})
+export class ProductModule { }
