e410257ffbfa1322d75ffa011ba37003a9b047fe
Thomas Robertz <thomas.robertz@googlemail.com>
2023-02-26 16:35:30 +0100

    Strangler: Decompose into own services

---
+++ udacity/cloud-native-application-architecture/3-message-passing/udaconnect/modules/api/app/location/api/controller.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from app.schema_repository.models import Connection, Location, Person
+from app.schema_repository.schemas import (
+    ConnectionSchema,
+    LocationSchema,
+    PersonSchema,
+)
+
+#from app.udaconnect.services import ConnectionService, LocationService, PersonService
+from flask import request
+from flask_accepts import accepts, responds
+from flask_restx import Namespace, Resource
+from typing import Optional, List
+
+DATE_FORMAT = "%Y-%m-%d"
+
+api = Namespace("UdaConnect-Location", description="Location CRUD services.")  # noqa
+
+@api.route("/locations")
+@api.route("/locations/<location_id>")
+@api.param("location_id", "Unique ID for a given Location", _in="query")
+class LocationResource(Resource):
+    @accepts(schema=LocationSchema)
+    @responds(schema=LocationSchema)
+    def post(self) -> Location:
+        request.get_json()
+        location: Location = LocationService.create(request.get_json())
+        return location
+
+    @responds(schema=LocationSchema)
+    def get(self, location_id) -> Location:
+        location: Location = LocationService.retrieve(location_id)
+        return location
