ba73391e25e78e65b388599f207785e0fd2a7dac
Thomas Robertz <thomas.robertz@adesso.de>
2024-01-13 18:36:24 +0100

    Comment.

--- udemy/java/maven/ch9/ch9/src/test/java/de/robertz/SUT_SurefirePOJOTest.java
+++ udemy/java/maven/ch9/ch9/src/test/java/de/robertz/SUT_SurefirePOJOTest.java
@@ -4,6 +4,8 @@ package de.robertz;
 // Surefire will pick up *Test classes and test* methods (configurable)
 // Target is mvn test
 
+// Since we also have JUnit in the classpath, Sonar will complain that there is no test in this class.
+
 public class SUT_SurefirePOJOTest {
 
 	public void testGetHello() {
