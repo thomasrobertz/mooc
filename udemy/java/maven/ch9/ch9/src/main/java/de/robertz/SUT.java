75bd57968de0851623467da6f362f213d93a4f4c
Thomas Robertz <thomas.robertz@adesso.de>
2024-01-13 18:23:53 +0100

    Surefire POJO Test.

---
+++ udemy/java/maven/ch9/ch9/src/main/java/de/robertz/SUT.java
@@ -0,0 +1,7 @@
+package de.robertz;
+
+public class SUT {
+	public String getHello() {
+		return "Hello World.";
+	}
+}
---
+++ udemy/java/maven/ch9/ch9/src/test/java/de/robertz/SUTTest.java
@@ -0,0 +1,13 @@
+package de.robertz;
+
+// A POJO Surefire test. You get this OOTB. No JUnit/other dependencies needed.
+// Surefire will pick up *Test classes and test* methods (configurable)
+// Target is mvn test
+
+public class SUTTest {
+
+	public void testGetHello() {
+		SUT s = new SUT();
+		assert("Hello World.".equals(s.getHello()));
+	}
+}
