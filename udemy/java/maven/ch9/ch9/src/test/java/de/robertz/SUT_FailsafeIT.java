9f7b6a915dbf23d291a491c75ce6dbf5b0bd2ec8
Thomas Robertz <thomas.robertz@adesso.de>
2024-01-14 18:22:43 +0100

    Comment.

--- udemy/java/maven/ch9/ch9/src/test/java/de/robertz/SUT_FailsafeIT.java
+++ udemy/java/maven/ch9/ch9/src/test/java/de/robertz/SUT_FailsafeIT.java
@@ -3,6 +3,9 @@ package de.robertz;
 import org.junit.jupiter.api.Test;
 
 public class SUT_FailsafeIT {
+
+	// Should work with goal mvn verify
+
 	@Test
 	void mockIT() {
 		System.out.println(">>>>> Integration Test <<<<<");
