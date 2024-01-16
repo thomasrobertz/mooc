e07b0600fd8f3453be298c29a99362379daddc1b
Thomas Robertz <thomas.robertz@adesso.de>
2024-01-16 13:47:27 +0100

    A simple maven multimodule project.

---
+++ udemy/java/maven/ch10/ch10/jpa-entities/src/main/java/entities/User.java
@@ -0,0 +1,23 @@
+package entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+	private String email;
+}
