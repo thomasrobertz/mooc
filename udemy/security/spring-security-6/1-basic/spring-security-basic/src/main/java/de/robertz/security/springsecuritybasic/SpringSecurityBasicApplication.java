8334231e842c0de7bcaf105633eb1e3daeb50c50
Thomas Robertz <thomas.robertz@adesso.de>
2024-03-26 16:41:03 +0100

    Comment

--- udemy/security/spring-security-6/1-basic/spring-security-basic/src/main/java/de/robertz/security/springsecuritybasic/SpringSecurityBasicApplication.java
+++ udemy/security/spring-security-6/1-basic/spring-security-basic/src/main/java/de/robertz/security/springsecuritybasic/SpringSecurityBasicApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityBasicApplication {
 
+	/*
+	* Spring will generate a one-time Password in the console.
+	* Go to http://localhost:8080/welcome
+	* Login: 	name: user
+	* 				password: the above generated
+	* */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityBasicApplication.class, args);
 	}
