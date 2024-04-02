0b8389f5fe08cf7d4e0391f14e6b8bcdfbd3f57e
Thomas Robertz <thomas.robertz@adesso.de>
2024-04-02 16:55:43 +0200

    Custom SecurityFilterChain.

---
+++ udemy/security/spring-security-6/2-eazybank/spring-security-2/src/main/java/de/robertz/security/eazybank/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package de.robertz.security.eazybank.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration // Just a standard configuration
+public class SecurityConfig {
+
+	// Copy-pasted from SecurityFilterChainConfiguration.
+	// In this commit, security will function exactly as before.
+	@Bean
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(rs -> rs.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
+}
