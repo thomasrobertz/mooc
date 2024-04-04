8482bfde3948740116c1b859666afa857432fa06
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2024-04-04 12:09:45 +0200

    Election with older zookeeper

--- udemy/distributed/ds_cloud/election/src/main/java/de/robertz/mooc/LeaderElection.java
+++ udemy/distributed/ds_cloud/election/src/main/java/de/robertz/mooc/LeaderElection.java
@@ -1,16 +1,43 @@
 package de.robertz.mooc;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 //@Slf4j
 public class LeaderElection implements Watcher {
     private final static String SERVER = "localhost:2181";
+    private final static String ELECTION_PATH = "/election";
     private ZooKeeper zk;
+
+    private String voteTicket; // The created ephemeral znode with this node's registration order
+
+    public void volunteer() throws InterruptedException, KeeperException {
+        // Create a znode to volunteer for election.
+        String znodePath = zk.create(ELECTION_PATH + "/c_",
+                new byte[] { }, // Content, empty in this case.
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, // We don't care about this here
+                CreateMode.EPHEMERAL_SEQUENTIAL); // Ephemeral = will be deleted once ZK is stopped
+
+        System.out.println(">>>>> Volunteered with vote ticket " + znodePath);
+        voteTicket = znodePath.replace(ELECTION_PATH + "/", "");
+    }
+
+    public void elect() throws InterruptedException, KeeperException {
+        List<String> children = zk.getChildren(ELECTION_PATH, false);
+        Collections.sort(children);
+        String leader = children.get(0);
+        if(leader.equals(voteTicket)) {
+            // The smallest registered vote is this node's ticket number
+            // therefore, this node has become the leader
+            System.out.println(">>>>> This node won the election process and is now leader.");
+        }
+        else {
+            System.out.println(">>>>> " + leader + " is the leader.");
+        }
+    }
 
     public LeaderElection connect() throws IOException {
         Watcher watcher = this; // To make clear why we're implementing Watcher
@@ -31,7 +58,7 @@ public class LeaderElection implements Watcher {
     @Override
     public void process(WatchedEvent watchedEvent) {
         switch(watchedEvent.getType()) {
-            case None -> {
+            case None:
                 if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
                     System.out.println("Successfully connected.");
                 } else {
@@ -40,7 +67,7 @@ public class LeaderElection implements Watcher {
                         zk.notifyAll();
                     }
                 }
-            }
         }
+
     }
 }
--- udemy/distributed/ds_cloud/election/src/main/java/de/robertz/mooc/Main.java
+++ udemy/distributed/ds_cloud/election/src/main/java/de/robertz/mooc/Main.java
@@ -1,11 +1,16 @@
 package de.robertz.mooc;
 
+import org.apache.zookeeper.KeeperException;
+
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection le = new LeaderElection();
-        le.connect().run();
+        le.connect();
+        le.volunteer();
+        le.elect();
+        le.run();
         le.close(); // Main thread woke up by notifyAll
         System.out.println("Disconnected.");
     }
