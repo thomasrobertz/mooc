f0bbf3b9b8adcb6aa306ecf77a8253c427891cb1
Thomas Robertz <thomas.robertz@adesso.de>
2024-06-12 16:03:55 +0200

    Inheritance

--- udemy/kotlin/introduction-to-kotlin/ch1/src/main/kotlin/Inheritance.kt
+++ udemy/kotlin/introduction-to-kotlin/ch1/src/main/kotlin/Inheritance.kt
@@ -9,12 +9,23 @@ fun main(a: Array<String>) {
     a.talk()
 }
 
-class Person(var name:String) {
+// Classes (and functions) are final by default,
+// to make them inheritable, add "open"
+open class Person(var name:String) {
     fun talk() {
         println("I am $name.")
     }
+    open fun overrideMe() {
+
+    }
 }
 
-class Manager(var name:String, var subordinates:List<Person>) {
-
+// Inherit from Person, and it is mandatory to call the parent cons, we
+// do this here with Person(mName)
+// We can't use "name" because that hides the supertype param.
+// We could add "override" to the field, but just as well rename as we did here.
+class Manager(var mName:String, var subordinates:List<Person>) : Person(mName) {
+    override fun overrideMe() {
+        super.overrideMe()
+    }
 }
