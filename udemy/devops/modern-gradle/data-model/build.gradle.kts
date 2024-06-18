41939f0f5927eb1f726ba3c1c91543cfa7cf3337
Thomas Robertz <thomas.robertz@adesso.de>
2024-06-18 17:03:54 +0200

    Fix data-model

--- udemy/devops/modern-gradle/data-model/build.gradle.kts
+++ udemy/devops/modern-gradle/data-model/build.gradle.kts
@@ -1,10 +1,3 @@
-// A clean gradle build file should start with a plugin block.
-// This also allows us to define plugins locally.
 plugins {
-    id("my-java-library") // Specifies a Java library module. Also applies id("java") which adds core Java functionality
-}
-
-java {
-    // This is an extension (Added by java-library above, if we remove it, this will not get recognized)
-    toolchain.languageVersion.set(JavaLanguageVersion.of(13))
+    id("my-java-library")
 }
--- udemy/devops/modern-gradle/gradle/plugins/java-plugins/build.gradle.kts
+++ udemy/devops/modern-gradle/gradle/plugins/java-plugins/build.gradle.kts
@@ -4,5 +4,6 @@ plugins {
 }
 
 dependencies {
+    // Add dependency
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.8.0")
 }
