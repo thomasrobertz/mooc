1280838931971d56d7416f314992e92925cc4cf9
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2024-11-18 15:50:04 +0100

    Added programmtic navigation (Click on a pie on home page to go directly to the pie)

--- pluralsight/angular/angular-routing-course/ch5/src/app/home/sub-section/card/card.component.ts
+++ pluralsight/angular/angular-routing-course/ch5/src/app/home/sub-section/card/card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Pie } from 'src/app/models/pie';
 import { CartService } from 'src/app/services/cart.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ROUTE_SEGMENTS } from 'src/app/app.routes';
 
 @Component({
   standalone: true,
@@ -15,4 +17,17 @@ import { CartService } from 'src/app/services/cart.service';
 export class CardComponent {
   @Input() pie!: Pie;
   readonly cartService = inject(CartService);
+  readonly router = inject(Router);
+
+  //readonly activatedRoute = inject(ActivatedRoute);
+
+  // Programmatic navigation
+  selectPie(pie: Pie) {
+    // We don't need to redeclare ROUTE_SEGMENTS here, we're just using as a string for
+    // programmatic navigation, not in a template.
+    this.router.navigate([`../${ROUTE_SEGMENTS.PRODUCTS}`], /*{
+      For future changes in route
+      relativeTo: this.activatedRoute
+    }*/);
+  }
 }
