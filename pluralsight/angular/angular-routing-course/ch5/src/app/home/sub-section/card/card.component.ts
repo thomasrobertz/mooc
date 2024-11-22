145ccca993fd9c4c53b2c58df802d429096d1cca
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2024-11-22 15:24:41 +0100

    Fix card component.

--- pluralsight/angular/angular-routing-course/ch5/src/app/home/sub-section/card/card.component.ts
+++ pluralsight/angular/angular-routing-course/ch5/src/app/home/sub-section/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Pie } from 'src/app/models/pie';
-import { CartService } from 'src/app/services/cart.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ROUTE_SEGMENTS } from 'src/app/app.routes';
+import { Pie } from 'src/app/models/pie';
+import { CartService } from 'src/app/services/cart.service';
+import { PieService } from 'src/app/services/pie.service';
 
 @Component({
   standalone: true,
@@ -18,16 +19,11 @@ export class CardComponent {
   @Input() pie!: Pie;
   readonly cartService = inject(CartService);
   readonly router = inject(Router);
+  readonly activatedRoute = inject(ActivatedRoute);
 
-  //readonly activatedRoute = inject(ActivatedRoute);
-
-  // Programmatic navigation
   selectPie(pie: Pie) {
-    // We don't need to redeclare ROUTE_SEGMENTS here, we're just using as a string for
-    // programmatic navigation, not in a template.
-    this.router.navigate([`../${ROUTE_SEGMENTS.PRODUCTS}`], /*{
-      For future changes in route
-      relativeTo: this.activatedRoute
-    }*/);
+    this.router.navigate([`../${ROUTE_SEGMENTS.PRODUCTS}`, pie.category], {
+      relativeTo: this.activatedRoute,
+    });
   }
 }
