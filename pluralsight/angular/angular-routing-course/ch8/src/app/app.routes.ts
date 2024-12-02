7c6a0df46a26327626d92812ec24ef0ae818c1cd
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2024-12-02 12:34:23 +0100

    Attempt to add a lazy route.

--- pluralsight/angular/angular-routing-course/ch8/src/app/app.routes.ts
+++ pluralsight/angular/angular-routing-course/ch8/src/app/app.routes.ts
@@ -37,7 +37,10 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.ABOUT,
-    component: AboutComponent,
+    // This won't work on its own, because it only tells ng which chunk js to load.
+    // We also need to add a child route to be appended to the router.
+    // We will do that in the AboutComponent.
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
   },
   {
     path: ROUTER_TOKENS.CHECKOUT,
