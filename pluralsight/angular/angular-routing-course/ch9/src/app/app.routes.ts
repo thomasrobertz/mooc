1f54e8238ece224b57c9073de68a72708cf96489
Thomas Robertz <thomasrobertz@users.noreply.github.com>
2024-12-02 13:40:44 +0100

    Route guards

--- pluralsight/angular/angular-routing-course/ch9/src/app/app.routes.ts
+++ pluralsight/angular/angular-routing-course/ch9/src/app/app.routes.ts
@@ -1,7 +1,16 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CartAuthGuard } from './cart-auth-route-guard';
+import { HomeUpdatedComponent } from './home-updated/home-updated.component';
+import { inject } from '@angular/core';
+import { FeatureFlagService } from './services/feature-flag.service';
+import { map } from 'rxjs';
+import { HelloService } from './services/hello.service';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './services/contact.service';
+import { authRouteGuard } from './cart-auth-route-guard';
+import { NotReadyComponent } from './not-ready/not-ready.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
 
 export enum ROUTER_TOKENS {
   HOME = 'home',
@@ -22,6 +31,16 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.HOME,
+    component: HomeUpdatedComponent,
+    canMatch: [() => {
+      const flagService = inject(FeatureFlagService);
+      return flagService.featureFlags.pipe(map((flag) => {
+        return !!flag.home
+      }))
+    }]
+  },
+  {
+    path: ROUTER_TOKENS.HOME,
     component: HomeComponent,
   },
   {
@@ -30,17 +49,44 @@ export const ROUTES: Routes = [
   },
   {
     path: ROUTER_TOKENS.CONTACT,
-    loadComponent: () => import('./contact/contact.component').then(m => m.ContactComponent)
+    component: ContactComponent,
+    canActivate: [
+      () => {
+        const flagService = inject(FeatureFlagService);
+        const router = inject(Router)
+
+        return flagService.featureFlags.pipe(map((flag) => !!flag.contact  || router.parseUrl(`/${ROUTER_TOKENS.NOT_READY}`)))
+      },
+      authRouteGuard(ROUTER_TOKENS.CONTACT)
+    ],
+    resolve: {userHello: () => {
+      const helloService = inject(HelloService);
+
+      return helloService.getUserHello()
+    }},
+    canDeactivate: [() => {
+      const contactService = inject(ContactService);
+      return contactService.canDeactivate();
+    }]
+  },
+  {
+    path: ROUTER_TOKENS.NOT_AUTH,
+    component: NotAuthorizedComponent,
+  },
+  {
+    path: ROUTER_TOKENS.NOT_READY,
+    component: NotReadyComponent,
   },
   {
     path: ROUTER_TOKENS.ABOUT,
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule),
   },
   {
+    // Use 'Lara' for login to see cart.
     path: ROUTER_TOKENS.CHECKOUT,
     outlet: ROUTER_TOKENS.CART,
     loadComponent: () => import('./cart/cart.component').then(m => m.CartComponent),
-    canActivate: [CartAuthGuard]
+    canActivate: [authRouteGuard(ROUTER_TOKENS.CART)]
   },
   {
     path: '**',
--- pluralsight/angular/angular-routing-course/ch9/src/app/cart-auth-route-guard.ts
+++ pluralsight/angular/angular-routing-course/ch9/src/app/cart-auth-route-guard.ts
@@ -1,16 +1,17 @@
-import { Injectable, inject } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { map } from 'rxjs';
 import { ROUTER_TOKENS } from './app.routes';
 
-@Injectable({ providedIn: 'root' })
-export class CartAuthGuard implements CanActivate {
-  private authService = inject(AuthService);
-  private router = inject(Router);
+export function authRouteGuard(route: string){
+  return () => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
 
-  canActivate() {
-    return this.authService.userAuth.pipe(
-      map((permissions) => !!permissions?.includes('cart') || this.router.parseUrl(`/${ROUTER_TOKENS.NOT_AUTH}`)))
+    return authService.userAuth.pipe(
+      map((permissions) =>
+        !!permissions?.includes(route) || router.parseUrl(`/${ROUTER_TOKENS.NOT_AUTH}`
+      )));
   }
 }
